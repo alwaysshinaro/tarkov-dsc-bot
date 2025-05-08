9db7816faef452946da36684fad10d52254a30b9
しんたろうくん <shintarouxxred@gmail.com>
2025-05-08 12:03:03 +0900

    fix

--- func/map-func.ts
+++ func/map-func.ts
@@ -1,6 +1,6 @@
 import { maps } from "../data/data";
 
-const getMaps = () => {
+const getMaps = (): string => {
   const randomIndex = Math.floor(Math.random() * maps.length);
   const map = maps[randomIndex];
   return map;
--- func/role-func.ts
+++ func/role-func.ts
@@ -1,6 +1,6 @@
 import {roles} from "../data/data";
 
-const getRoles = () => {
+const getRoles = (): string => {
     const randomIndex = Math.floor(Math.random() * roles.length);
     const role = roles[randomIndex];
     return role;
